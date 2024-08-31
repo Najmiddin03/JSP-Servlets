e9184bd6e625530ddf9dbb85de3801101b70c0be
Najmiddin <nazarmatovn03@gmail.com>
2024-08-31 21:27:14 +0200

    session in jsp

---
+++ SessionInJSP/src/main/java/org/neotron/SiteController.java
@@ -0,0 +1,43 @@
+package org.neotron;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SiteController
+ */
+@WebServlet("/SiteController")
+public class SiteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public SiteController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if (username.equals("Nedji") && password.equals("5656")) {
+			request.getSession().invalidate();
+			HttpSession newSession = request.getSession();
+			newSession.setMaxInactiveInterval(500);
+			response.sendRedirect("member.jsp");
+		} else {
+			response.sendRedirect("login.jsp");
+		}
+	}
+
+}
